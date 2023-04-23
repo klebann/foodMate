e0e6d14ab26b7a72bc467826ee4ebd6dd8e38b95
Jakub Kleban <jakub.kleban2000@gmail.com>
2023-04-23 19:59:07 +0200

    Default polish language

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -12,14 +12,12 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+//    public function configureFields(string $pageName): iterable
+//    {
+//        return [
+//            IdField::new('id'),
+//            TextField::new('name'),
+//            TextEditorField::new('description'),
+//        ];
+//    }
 }
