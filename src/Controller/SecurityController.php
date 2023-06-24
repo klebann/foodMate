f62f6bc8bc92304b65c505c3fa21977dd802b8b8
Jakub Kleban <jakub.kleban2000@gmail.com>
2023-06-24 09:18:30 +0200

    Update frontend

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,6 +24,21 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
+    #[Route(path: '/admin/register', name: 'app_register')]
+    public function register(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/register.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
     #[Route(path: '/admin/logout', name: 'app_logout')]
     public function logout(): void
     {
