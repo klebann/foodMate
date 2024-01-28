eceef91a628ea6b0068d68eb9e1c28a6145d9e1a
Jakub Kleban <jakub.kleban@cairo.pl>
2024-01-28 22:47:05 +0100

    Add backend to calendar.

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -7,7 +7,6 @@ use App\Entity\Recipe;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DataTransformerChain;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,30 +15,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class CalendarController extends AbstractController
 {
     #[Route('/calendar/{year}/{month}/{day}', name: 'app_calendar')]
-    #[Template('calendar/index.html.twig')]
-    public function index(EntityManagerInterface $em, Request $request, string $year = "0", string $month = "0", string $day = "0"): array
+    public function index(EntityManagerInterface $em, Request $request, string $year = "0", string $month = "0", string $day = "0"): \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
     {
         if ($year === "0") {
             $year = date('Y');
             $month = date('m');
             $day = date('d');
         }
+        $date = new \DateTime("{$year}-{$month}-{$day}");
 
         $meal = new Meal();
         $meal->setUser($this->getUser());
-        $meal->setDate(new \DateTime("{$year}-{$month}-{$day}"));
-
+        $meal->setDate($date);
         $recipes = $em->getRepository(Recipe::class)->findAll();
 
         $form = $this->createFormBuilder($meal)
             ->add('type', ChoiceType::class, [
                 'choices' => [
-                    'Śniadanie' => '1',
-                    'II Śniadanie' => '2',
-                    'Lunch' => '3',
-                    'Obiad' => '4',
-                    'Przekąska' => '5',
-                    'Kolacja' => '6'
+                    'Śniadanie' => 'Śniadanie',
+                    'II Śniadanie' => 'II Śniadanie',
+                    'Lunch' => 'Lunch',
+                    'Obiad' => 'Obiad',
+                    'Przekąska' => 'Przekąska',
+                    'Kolacja' => 'Kolacja'
                 ]
             ])
             ->add('recipe', ChoiceType::class, [
@@ -55,11 +53,35 @@ class CalendarController extends AbstractController
 
             $em->persist($meal);
             $em->flush();
+
+            return $this->redirectToRoute('app_calendar', [
+                'day' => $day,
+                'month' => $month,
+                'year' => $year
+            ]);
         }
 
-        return [
+        $meals = $em->getRepository(Meal::class)->findBy([
+            'date' => $date,
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('calendar/index.html.twig', [
             'form' => $form,
-            'recipes' => $recipes
-        ];
+            'recipes' => $recipes,
+            'day' => $day,
+            'month' => $month,
+            'year' => $year,
+            'meals' => $meals
+        ]);
+    }
+
+    #[Route('/meal/{meal}/delete', name: 'delete_meal', options: ['expose' => true])]
+    #[Template('calendar/index.html.twig')]
+    public function delete(EntityManagerInterface $em, Meal $meal): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+        $em->remove($meal);
+        $em->flush();
+        return $this->redirectToRoute('app_calendar');
     }
 }
